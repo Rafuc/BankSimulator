020e3f6de5c93ccf83c13de8a101d4e78be3f7f2
Kamil Kot <kamil.kot07@gmail.com>
2019-02-13 20:55:55 +0100

    Added Transactions :D

--- BankSimulator/Program.cs
+++ BankSimulator/Program.cs
@@ -9,9 +9,24 @@ namespace BankSimulator
         static void Main(string[] args)
         {
             StoreContext storeContext = new StoreContext();
-
             LoginSystem _loginSystem = new LoginSystem(storeContext);
-            _loginSystem.HelloMesseges();
+            HomePage _mainFuctions = new HomePage(storeContext);
+            _mainFuctions.SetStoreContext(storeContext);
+
+            while(!_loginSystem.userIsLogged)
+            {
+                _loginSystem.HelloMesseges();
+            }
+
+            while(!_mainFuctions.isLogged)
+            {
+                _mainFuctions.StartPage();
+
+                if(_mainFuctions.ReturnChoose() == 1)
+                {
+                    _mainFuctions.Transfer();
+                }
+            }
 
             Console.ReadKey();
         }
