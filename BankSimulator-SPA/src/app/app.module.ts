7c7b652fccf38fe8406ca85dfd8041b909e1b55e
Kamil Kot <kamil.kot07@gmail.com>
2019-02-17 19:04:16 +0100

    front-end

--- BankSimulator-SPA/src/app/app.module.ts
+++ BankSimulator-SPA/src/app/app.module.ts
@@ -1,43 +1,46 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-import {BrowserAnimationsModule, NoopAnimationsModule} from '@angular/platform-browser/animations';
-import {MatButtonModule, MatCheckboxModule, MatGridListModule, MatCardModule, MatMenuModule, MatIconModule, MatInputModule, MatSelectModule, MatRadioModule} from '@angular/material';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
 
+import { AppRoutingModule } from './app.routing';
+import { ComponentsModule } from './components/components.module';
 
 import { AppComponent } from './app.component';
-import { LayoutModule } from '@angular/cdk/layout';
-import { ReactiveFormsModule } from '@angular/forms';
-import { LoginComponent } from './login/login.component';
 
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
+import { TableListComponent } from './table-list/table-list.component';
+import { TypographyComponent } from './typography/typography.component';
+import { IconsComponent } from './icons/icons.component';
+import { MapsComponent } from './maps/maps.component';
+import { NotificationsComponent } from './notifications/notifications.component';
+import { UpgradeComponent } from './upgrade/upgrade.component';
+import {
+  AgmCoreModule
+} from '@agm/core';
+import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
 
 @NgModule({
-   declarations: [
-      AppComponent,
-      LoginComponent,
-      
-   ],
-   imports: [
-      BrowserAnimationsModule,
-      NoopAnimationsModule,
-      MatButtonModule, 
-      MatCheckboxModule,
-      BrowserModule,
-      MatGridListModule,
-      MatCardModule,
-      MatMenuModule,
-      MatIconModule,
-      LayoutModule,
-      MatInputModule,
-      MatSelectModule,
-      MatRadioModule,
-      ReactiveFormsModule
-   ],
-   providers: [],
-   bootstrap: [
-      AppComponent
-   ]
+  imports: [
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpModule,
+    ComponentsModule,
+    RouterModule,
+    AppRoutingModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'YOUR_GOOGLE_MAPS_API_KEY'
+    })
+  ],
+  declarations: [
+    AppComponent,
+    AdminLayoutComponent,
+
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
-
-export class PizzaPartyAppModule { }
